ec0afbaffe6295d3429ee042de3bab503ede0966
Andrew Sansum <sansumandrew@gmail.com>
2025-07-22 12:18:50 +0100

    fix: Correct package name

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name="orangefox", version="0.1.0", install_requires=["pygame-ce"])
+setup(name="orangefoxengine", version="0.1.0", install_requires=["pygame-ce"])
